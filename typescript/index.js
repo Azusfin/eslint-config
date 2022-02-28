be5ce06d09419bce1175538ee0f8a636181cc247
Azusfin <Azusfin@users.noreply.github.com>
2022-02-28 12:54:53 +0700

    Initial Commit

---
+++ typescript/index.js
@@ -0,0 +1,44 @@
+/* eslint-env node */
+const { resolve } = require("path")
+
+module.exports = {
+    extends: [resolve(__dirname, "..", "index.js"), resolve(require.resolve("@rahagia/eslint-config/typescript"))],
+    rules: {
+        "@typescript-eslint/no-dynamic-delete": "off",
+        "@typescript-eslint/explicit-member-accessibility": ["error", {
+            accessibility: "explicit",
+            overrides: {
+                constructors: "no-public"
+            }
+        }],
+        "@typescript-eslint/member-ordering": "off",
+        "@typescript-eslint/naming-convention": ["warn", [{
+            selector: "default",
+            format: ["camelCase"],
+            leadingUnderscore: "forbid",
+            trailingUnderscore: "forbid"
+        }, {
+            selector: "variable",
+            format: ["camelCase", "UPPER_CASE"]
+        }, {
+            selector: "typeLike",
+            format: ["PascalCase"]
+        }, {
+            selector: "property",
+            format: ["camelCase", "UPPER_CASE"],
+            leadingUnderscore: "allowSingleOrDouble"
+        }]],
+        "@typescript-eslint/member-delimiter-style": ["warn", {
+            multiline: {
+                delimiter: "none",
+                requireLast: false
+            },
+            singleline: {
+                delimiter: "semi",
+                requireLast: false
+            }
+        }],
+        "@typescript-eslint/semi": "off",
+        ...require(resolve(__dirname, "..", "index.js")).rules
+    }
+}
